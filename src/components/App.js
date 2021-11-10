5a5468132ea9f32411cb53de12e79209fc8884e0
Jo <jo-lion@hotmail.fr>
2021-11-10 15:02:41 +0100

    react-susp

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import Navbar from './Navbar.jsx';
+import MainPage from './MainPage.jsx'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,11 @@
+import icon from '../assets/icon.png'
+
+function MainPage() {
+  return (
+    <div className="mt-10">
+      <img src={icon} alt="icon réglage susp" className="w-1/3 mx-auto" />
+    </div>
+  );
+}
+
+export default MainPage;
